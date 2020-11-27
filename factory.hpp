f0636ad2707f8b2b47837b16757f9736fc8a3dc2
Daniel Nissan <danielnissan@Daniels-MBP.attlocal.net>
2020-11-27 01:03:09 -0800

    initial steps

---
+++ factory.hpp
@@ -0,0 +1,16 @@
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
+
+#include "lab4/base.hpp"
+
+class Factory {
+    private:
+    public:
+        Factory(){};
+        Base* parse(char** input, int length){
+            
+        };
+
+};
+
+#endif
