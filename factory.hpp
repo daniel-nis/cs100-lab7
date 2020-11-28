417237e3c78c62c82b76218068538c7c79554637
Daniel Nissan <danielnissan@Daniels-MBP.attlocal.net>
2020-11-27 18:25:28 -0800

    working on algorithm

--- factory.hpp
+++ factory.hpp
@@ -1,13 +1,53 @@
 #ifndef __FACTORY_HPP__
 #define __FACTORY_HPP__
 
+#include <iostream>
+#include <iterator>
 #include "lab4/base.hpp"
+#include "lab4/add.hpp"
+#include "lab4/sub.hpp"
+#include "lab4/div.hpp"
+#include "lab4/mult.hpp"
+#include "lab4/power.hpp"
+#include <string>
+
+using namespace std;
 
 class Factory {
     private:
     public:
         Factory(){};
         Base* parse(char** input, int length){
+
+            cout << "Enter calculation: " << endl;
+            string calc;
+            getline(cin, calc);
+
+            while(calc.next() != endl){
+                string op1 = calc.next();
+
+                if(calc.next() = "+"){
+                    string op2 = calc.next();
+                    Base* add = new Add(op1, op2);
+                }
+                /*
+                else if(input.next() = "-"){
+                    string op2 = input.next();
+                    Base* sub = new Sub(op1, op2);
+                }
+                else if(input.next() = "/"){
+                    string op2 = input.next();
+                    Base* div = new Div(op1, op2);
+                }
+                else if(input.next() = "*"){
+                    string op2 = input.next();
+                    Base* mult = new Mult(op1, op2);
+                }
+                else if(input.next() = "**"){
+                    string op2 = input.next();
+                    Base* pow = new Power(op1, op2);
+                }*/
+            }
             
         };
 
